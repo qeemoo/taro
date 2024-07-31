4a4656c22048fc82275ffc3aa1523c2ee327eeff
qeemoo <gyooheon2@gamil.com>
2024-07-31 18:51:07 +0900

    ✨ Feat: 카드 목록 데이터 입력
    카드 목록 입력

---
+++ src/app/information/card/page.tsx
@@ -0,0 +1,118 @@
+const Card = () => {
+  return (
+    <div>
+      <div className="flex gap-20">
+        <div className="flex flex-col">
+          <div>메이저 아르카나</div>
+          <ul>
+            <li>0.광대</li>
+            <li>1.마법사</li>
+            <li>2.여사제</li>
+            <li>3.여왕</li>
+            <li>4.황제</li>
+            <li>5.교황</li>
+            <li>6.연인들</li>
+            <li>7.전차</li>
+            <li>8.힘</li>
+            <li>9.은둔자</li>
+            <li>10.운명의 수레바퀴</li>
+            <li>11.정의</li>
+            <li>12.매달린 사람</li>
+            <li>13.죽음</li>
+            <li>14.절제</li>
+            <li>15.악마</li>
+            <li>16.탑</li>
+            <li>17.별</li>
+            <li>18.달</li>
+            <li>19.해</li>
+            <li>20.심판</li>
+            <li>21.세계</li>
+          </ul>
+        </div>
+        <div className="flex flex-col gap-10">
+          <div>마이너 아르카나</div>
+          <div className="flex gap-10">
+            <div>
+              <div>막대기(지팡이)</div>
+              <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+                <li>6</li>
+                <li>7</li>
+                <li>8</li>
+                <li>9</li>
+                <li>10</li>
+                <li>시종</li>
+                <li>기사</li>
+                <li>여왕</li>
+                <li>왕</li>
+              </ul>
+            </div>
+            <div>
+              <div>검</div>
+              <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+                <li>6</li>
+                <li>7</li>
+                <li>8</li>
+                <li>9</li>
+                <li>10</li>
+                <li>시종</li>
+                <li>기사</li>
+                <li>여왕</li>
+                <li>왕</li>
+              </ul>
+            </div>
+            <div>
+              <div>잔(성배)</div>
+              <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+                <li>6</li>
+                <li>7</li>
+                <li>8</li>
+                <li>9</li>
+                <li>10</li>
+                <li>시종</li>
+                <li>기사</li>
+                <li>여왕</li>
+                <li>왕</li>
+              </ul>
+            </div>
+            <div>
+              <div>동전(디스크)</div>
+              <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+                <li>6</li>
+                <li>7</li>
+                <li>8</li>
+                <li>9</li>
+                <li>10</li>
+                <li>시종</li>
+                <li>기사</li>
+                <li>여왕</li>
+                <li>왕</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/app/information/page.tsx
+++ src/app/information/page.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link';
+
 const Information = () => {
   return (
-    <section>
-      <div>sdsd</div>
+    <section className="flex h-full items-center justify-center gap-10">
+      <Link href="/information/card" className="h-96 w-96 bg-pink-300">
+        카드
+      </Link>
+      <Link href="/information/spread" className="h-96 w-96 bg-pink-300">
+        스프레드
+      </Link>
     </section>
   );
 };
