37193c1dfb48d26843d13977d62cdaf013e34708
qeemoo <gyooheon2@gamil.com>
2024-08-04 10:52:52 +0900

    ✨ Feat: 스프레드 페이지 생성
    스프레드 페이지 생성

---
+++ src/app/(content)/study/spread/page.tsx
@@ -0,0 +1,13 @@
+const Spread = () => {
+  return (
+    <section>
+      <div>원 카드 스프레드</div>
+      <div>쓰리 카드 스프레드</div>
+      <div>켈트 십자가 스프레드</div>
+      <div>생명의 나무 스프레드</div>
+      <div>말 발굽 스프레드</div>
+      <div>보름달 스프레드</div>
+    </section>
+  );
+};
+export default Spread;
